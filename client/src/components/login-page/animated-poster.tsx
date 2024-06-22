858b9c470b335ac29d447336c258ef7bd8184159
Misho <chapidze.misho95@gmail.com>
2024-06-23 01:26:07 +0400

    sss

---
+++ client/src/components/login-page/animated-poster.tsx
@@ -0,0 +1,5 @@
+const AnimatedPoster = () => {
+  return <div></div>;
+};
+
+export default AnimatedPoster;
--- client/src/pages/login-page.tsx
+++ client/src/pages/login-page.tsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 const LoginPage = () => {
-  return <main>Login Page</main>;
+  return <MainComponent>Login Page</MainComponent>;
 };
+
+const MainComponent = styled.main`
+  display: flex;
+  min-width: 100vw;
+  min-height: 100vh;
+`;
 
 export default LoginPage;
